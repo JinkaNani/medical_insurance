03fd3e2354138d4050cf3acf2d88684833979243
JinkaNani <117426985+JinkaNani@users.noreply.github.com>
2022-11-04 11:50:22 +0530

    Add files via upload

---
+++ mdps_public.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  8 21:01:15 2022
+
+@author: siddhardhan
+"""
+
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+
+# loading the saved models
+
+
+Insurance_model = pickle.load(open('Medical_insurance.sav', 'rb'))
+
+
+
+#************************************************************************************************************************    
+
+# Medical_insurance Prediction Page
+    
+# page title
+st.title('Diabetes Prediction using ML')
+
+#age,sex,bmi,children,smoker,region,charges    
+    
+#getting the input data from the user
+col1, col2, col3 = st.columns(3)
+    
+with col1:
+age = st.text_input('age')
+        
+with col2:
+sex = st.text_input('sex')
+    
+with col3:
+bmi = st.text_input('bmi')
+    
+with col1:
+children = st.text_input('children')
+    
+with col2:
+smoker = st.text_input('smoker')
+    
+with col3:
+region = st.text_input('region')
+   
+    
+# code for Prediction
+medical_insurance_cost = ''
+    
+# creating a button for Prediction
+#age,sex,bmi,children,smoker,region,charges
+    
+if st.button('Diabetes Test Result'):
+	cost_prediction = Medical_insurance.predict([[age,sex,bmi,children,smoker,region]])
+        
+        
+st.success(cost_prediction)
+
+
+#********************************************************************************************************************************************************
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
